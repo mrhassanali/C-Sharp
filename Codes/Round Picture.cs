85206074af36fd1345923df793ffaf059149fd14
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-01-31 22:31:52 +0500

    Create Round Picture.cs
    Using this code you are able to Round the PictureBox in Window form C# Application.

---
+++ Codes/Round Picture.cs
@@ -0,0 +1,7 @@
+/* Adding this code on the click on the picture box then put on picture box event. */
+
+
+ System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, pictureBox1.Width - 3, pictureBox1.Height - 3);
+            Region rg = new Region(gp);
+            pictureBox1.Region = rg;
