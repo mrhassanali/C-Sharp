8de30575ed49d1fed3f8dbc19ba6a73e69dc86e3
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-08-01 13:56:40 +0500

    Add files via upload

---
+++ TexttoSpeech/TexttoSpeech/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace TexttoSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gender = new MetroFramework.Controls.MetroComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkViolet;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(23, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(492, 271);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Type a Text..";
+            this.textBox1.Enter += new System.EventHandler(this.textbox1_enter);
+            this.textBox1.Leave += new System.EventHandler(this.textbox1_leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(345, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Speech";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gender";
+            // 
+            // gender
+            // 
+            this.gender.FormattingEnabled = true;
+            this.gender.ItemHeight = 23;
+            this.gender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Natural"});
+            this.gender.Location = new System.Drawing.Point(159, 385);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(121, 29);
+            this.gender.TabIndex = 4;
+            this.gender.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 450);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
+            this.Text = "Text to Speech Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroComboBox gender;
+    }
+}
+
---
+++ TexttoSpeech/TexttoSpeech/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace TexttoSpeech
+{
+    public partial class Form1 :MetroFramework.Forms.MetroForm
+    {
+        SpeechSynthesizer sp = new SpeechSynthesizer(); //creating Object
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(gender.Text == "Male")
+            {
+                sp.SelectVoiceByHints(VoiceGender.Male);
+            }
+            if (gender.Text == "Female")
+            {
+                sp.SelectVoiceByHints(VoiceGender.Female);
+            }
+            if (gender.Text == "Natural")
+            {
+                sp.SelectVoiceByHints(VoiceGender.Neutral);
+            }
+            sp.Speak(textBox1.Text);
+        }
+
+        private void textbox1_enter(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Type a Text..")
+            {
+                textBox1.Clear();
+            }
+        }
+
+        private void textbox1_leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Type a Text..";
+            }
+        }
+    }
+}
